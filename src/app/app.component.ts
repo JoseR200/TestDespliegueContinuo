33103334531c980354ae2867d4ec58bee039697e
JoseR200 <85463913+JoseR200@users.noreply.github.com>
2025-02-28 17:56:29 +0100

    Test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'TestDespliegueContinuo';
+  title = 'TestDespliegueContinuo Prueba 2';
 }
